20b6cb15fe803a0eecfcdd986abfa0450db73cb9
batcha333 <mafktkader@gmail.com>
2022-11-12 16:20:39 +0530

    db commit

---
+++ db/DatabaseDemo.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+public class DatabaseDemo {
+    public static void main(String[] args) throws Exception {
+        Connection con = null;
+        Statement smt = null;
+        try{
+            System.out.println("Registering the Driver..........");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/firstdb", "root", "");
+            System.out.println("Connected..........");
+            smt=con.createStatement();
+            ResultSet rs = smt.executeQuery("SELECT * FROM mytable;");
+	    //int n=smt.executeUpdate("INSERT into emp values(1200,'MariShankar',25000.0)");
+	    //if(n>=0)
+		//System.out.println("One row updated.........");
+            System.out.println("ResultSet generated..........");
+           
+            while(rs.next())
+            {
+                System.out.println("***********************************");
+                System.out.println("Emp_ID-->"+rs.getInt(1));
+                System.out.println("Emp_Name-->"+rs.getString(2));
+		    System.out.println("Emp_Name-->"+rs.getString(3));
+                System.out.println("Emp_Salary-->"+rs.getDouble(4));
+            }
+            smt.close();
+            con.close();
+        }catch(Exception e)
+        {
+           e.getMessage();
+        }
+    }
+}
